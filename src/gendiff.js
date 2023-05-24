5fd016b118e74ac04b88a4076cb6da3c62f91112
Lukatar <lukatar@Ubuntu.homet.virtualbox.org>
2023-05-24 19:47:16 +0300

    Step #2 -gendiff add option and arguments

--- src/gendiff.js
+++ src/gendiff.js
@@ -4,6 +4,13 @@ const { program } = require('commander');
 
 program
   .version('1.0.0')
-  .description("Compares two configuration files and shows a difference.");
-
+  .description("Compares two configuration files and shows a difference.")
+  .option('-f, --format <type>', 'output format (default: "stylish")','stylish')
+  .arguments('<filepath1> <filepath2>');
 program.parse();
+
+const options = program.opts();
+const format = options.format;
+const filePath1 = program.args[0];
+const filePath2 = program.args[1];
+console.log(format,filePath1,filePath2);
