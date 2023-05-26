6f08906989fbe6b63a6097af3b0e3ca1f11c3f9f
Lukatar <lukatar@Ubuntu.homet.virtualbox.org>
2023-05-26 19:24:31 +0300

    Commit #3

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/jLukatar/frontend-project-46/workflows/hexlet-check/badge.svg)](https://github.com/jLukatar/frontend-project-46/actions)
+launch:
+```
+brain-even
+```
+If the number is even - you need to answer "yes", otherwise - "no".
+
+[![asciicast](https://asciinema.org/a/t5TfrobUDWLcj2NvtMT2gYHTT.svg)](https://asciinema.org/a/t5TfrobUDWLcj2NvtMT2gYHTT)
+<script async id="asciicast-xMZOeWLi7ORmWgByS9I9ekTzG" src="https://asciinema.org/a/xMZOeWLi7ORmWgByS9I9ekTzG.js"></script>
---
+++ bin/gendiff.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+import path from 'path';
+import * as fs from 'fs';
+import { program } from 'commander';
+import _ from 'lodash';
+
+program
+  .version('1.0.0')
+  .description("Compares two configuration files and shows a difference.")
+  .option('-f, --format <type>', 'output format','stylish')
+  .arguments('<filepath1> <filepath2>');
+program.parse();
+
+const newWorkingDirectory = path.resolve(process.cwd()+"/frontend-project-46");
+process.chdir(newWorkingDirectory);
+
+const filePath1 = path.resolve(process.cwd(), program.args[0]);
+const filePath2 = path.resolve(process.cwd(), program.args[1]);
+
+const file1 = JSON.parse(fs.readFileSync(filePath1, 'utf-8'));
+const file2 = JSON.parse(fs.readFileSync(filePath2, 'utf-8'));
+
+const genDiff = (data1, data2) => {
+  const space = " "; 
+  const mergedData = {...data1, ...data2};
+  const keys = Object.keys(mergedData);
+  const result = _.orderBy(keys).map((key) => {
+    if (!Object.hasOwn(data1, key)) {
+        return `${space.repeat(2)}+ ${key}: ${data2[key]}`;
+      } 
+      if (!Object.hasOwn(data2, key)) {
+        return `${space.repeat(2)}- ${key}: ${data1[key]}`;
+      }
+      if (data1[key] !== data2[key]) {
+        return `${space.repeat(2)}- ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`; 
+      }
+      return `${space.repeat(4)}${key}: ${data1[key]}`
+    });
+    return `{\n${result.join('\n')}\n}`;
+  }
+console.log(genDiff(file1,file2));
+console.log(newWorkingDirectory);
