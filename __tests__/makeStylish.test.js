512c5d8ef3556e4746d26ee47641a1de2e7a0e44
Lukatar <lukatar@Ubuntu.homet.virtualbox.org>
2023-08-28 15:15:16 +0300

    commit

--- __tests__/makeStylish.test.js
+++ __tests__/makeStylish.test.js
@@ -1,8 +1,7 @@
+/* eslint-disable import/extensions */
 import fs from 'fs/promises';
 import makeStylish from '../src/makeStylish.js';
 import sampleInput from '../__fixtures__/diffArray.js';
-
-
 
 describe('makeStylish', () => {
   test('should convert diff object to stylish string format', async () => {
